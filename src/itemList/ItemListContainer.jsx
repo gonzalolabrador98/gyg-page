6752ddd0a3c81b5166e5652a8c6e48d36cd550a6
unknown <gonzalolabrador98@gmail.com>
2022-09-05 23:59:16 -0300

    cards agregadas

--- src/itemList/ItemListContainer.jsx
+++ src/itemList/ItemListContainer.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+import { items } from '../utils/items';
 
 export const ItemListContainer = () => {
-  return (
-    <div>Aquí van las cards, titulos, descripcion, etc de nuestro catalogo</div>
-  );
+  const [productos, setProductos] = useState([]);
+
+  function consultarPromesa(confirmacion) {
+    return new Promise((res, rej) => {
+      if (confirmacion) {
+        res(items);
+      } else {
+        rej('Acceso denegado');
+      }
+    });
+  }
+
+  consultarPromesa(true)
+    .then((data) => {
+      const productosJSX = data.map((item, idx) => {
+        <div key={idx} className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="" alt="" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{item.nombre}</div>
+            <p className="text-gray-700 text-base">{item.marca}</p>
+            <p className="text-gray-700 text-base">{item.stock}</p>
+            <p className="text-gray-700 text-base">{item.precio}</p>
+          </div>
+        </div>;
+      });
+      setProductos(productosJSX);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return <>{productos}</>;
 };
---
+++ src/utils/items.js
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    id: 1,
+    nombre: 'Papel Aluminio Make',
+    marca: 'Make',
+    stock: 10,
+    precio: 255,
+  },
+  {
+    id: 2,
+    nombre: 'Papel Manteca Make',
+    marca: 'Make',
+    stock: 10,
+    precio: 315,
+  },
+  {
+    id: 3,
+    nombre: 'Film Adherente Make',
+    marca: 'Make',
+    stock: 10,
+    precio: 350,
+  },
+  {
+    id: 4,
+    nombre: 'Esponja acero 50gr Make',
+    marca: 'Make',
+    stock: 255,
+    precio: 70,
+  },
+  { id: 5, nombre: 'Matamoscas Make', marca: 'Make', stock: 40, precio: 130 },
+  {
+    id: 6,
+    nombre: 'Centrifugador de verduras Make',
+    marca: 'Make',
+    stock: 5,
+    precio: 680,
+  },
+];
