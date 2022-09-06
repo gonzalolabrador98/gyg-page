08bafdfb839687b1782e93d1a9cada07d994a30a
unknown <gonzalolabrador98@gmail.com>
2022-09-06 00:32:00 -0300

    cards agregadas1

--- src/itemList/ItemListContainer.jsx
+++ src/itemList/ItemListContainer.jsx
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { items } from '../utils/items';
+
+function consultarPromesa(confirmacion) {
+  return new Promise((res, rej) => {
+    if (confirmacion) {
+      res(items);
+    } else {
+      rej('Acceso denegado');
+    }
+  });
+}
 
 export const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
 
-  function consultarPromesa(confirmacion) {
-    return new Promise((res, rej) => {
-      if (confirmacion) {
-        res(items);
-      } else {
-        rej('Acceso denegado');
-      }
-    });
-  }
-
-  consultarPromesa(true)
-    .then((data) => {
-      const productosJSX = data.map((item, idx) => {
-        <div key={idx} className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img className="w-full" src="" alt="" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{item.nombre}</div>
-            <p className="text-gray-700 text-base">{item.marca}</p>
-            <p className="text-gray-700 text-base">{item.stock}</p>
-            <p className="text-gray-700 text-base">{item.precio}</p>
-          </div>
-        </div>;
+  useEffect(() => {
+    consultarPromesa(true)
+      .then((data) => {
+        setProductos(data);
+        console.log(productos);
+      })
+      .catch((error) => {
+        console.error(error);
       });
-      setProductos(productosJSX);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  }, []);
 
-  return <>{productos}</>;
+  return (
+    <>
+      {productos.map((item, idx) => (
+        <div
+          key={idx}
+          className="max-w-sm rounded overflow-hidden shadow-lg m-2"
+        >
+          <img className="w-full" src="" alt="" />
+          <div className=" px-6 py-4">
+            <div className="font-bold text-xl mb-2">Nombre: {item.nombre}</div>
+            <p className="text-gray-700 text-base">Marca: {item.marca}</p>
+            <p className="text-gray-700 text-base">Stock: {item.stock}</p>
+            <p className="text-gray-700 text-base">Precio: ${item.precio}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
